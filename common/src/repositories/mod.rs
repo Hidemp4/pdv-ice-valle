0788a002fd9010b3df9865b9abbd8ebd4c427792
cainhooow <augustocaio663@gmail.com>
2025-08-29 12:04:45 -0300

    feat:Repository para stock estruturado

--- common/src/repositories/mod.rs
+++ common/src/repositories/mod.rs
@@ -1,2 +1,3 @@
-pub mod product_repository;
 pub mod category_repository;
+pub mod product_repository;
+pub mod stock_repository;
---
+++ common/src/repositories/stock_repository.rs
@@ -0,0 +1,76 @@
+use std::sync::Arc;
+
+use crate::models::schema::stock::dsl::*;
+use crate::{
+    infrastructure::DbPool,
+    models::stock::{NewStock, Stock},
+};
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper, result::Error};
+
+pub struct DStockRepository {
+    pool: Arc<DbPool>,
+}
+
+impl DStockRepository {
+    pub fn new(pool: Arc<DbPool>) -> Self {
+        Self { pool }
+    }
+}
+
+pub trait StockRepository {
+    fn all(&self) -> Result<Vec<Stock>, Error>;
+    fn save(&self, nstock: &NewStock) -> Result<Stock, Error>;
+    fn update(&self, ustock: &Stock) -> Result<Stock, Error>;
+    fn find_by_id(&self, stock_id: i32) -> Result<Stock, Error>;
+}
+
+impl StockRepository for DStockRepository {
+    fn all(&self) -> Result<Vec<Stock>, Error> {
+        let mut conn = self.pool.get().unwrap();
+
+        match stock.select(Stock::as_select()).get_results(&mut conn) {
+            Ok(data) => Ok(data),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn save(&self, nstock: &NewStock) -> Result<Stock, Error> {
+        let mut conn = self.pool.get().unwrap();
+
+        match diesel::insert_into(stock)
+            .values(nstock)
+            .returning(Stock::as_returning())
+            .get_result(&mut conn)
+        {
+            Ok(data) => Ok(data),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn update(&self, ustock: &Stock) -> Result<Stock, Error> {
+        let mut conn = self.pool.get().unwrap();
+
+        match diesel::update(stock)
+            .filter(id.eq(ustock.id))
+            .set(ustock)
+            .returning(Stock::as_returning())
+            .get_result(&mut conn)
+        {
+            Ok(data) => Ok(data),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn find_by_id(&self, stock_id: i32) -> Result<Stock, Error> {
+        let mut conn = self.pool.get().unwrap();
+
+        match stock
+            .filter(id.eq(stock_id))
+            .select(Stock::as_select())
+            .get_result(&mut conn)
+        {
+            Ok(data) => Ok(data),
+            Err(err) => Err(err),
+        }
+    }
+}
